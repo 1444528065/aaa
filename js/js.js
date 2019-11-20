e25abf95f73d35eed47a9ddf68af30b5564ea092
1444528065 <1444528065@qq.com>
2019-11-20 22:56:03 +0800

    11

--- js/js.js
+++ js/js.js
@@ -1,20 +1,18 @@
 var ctboxs=document.getElementById('ct1');
 var ctbox=ctboxs.getElementsByClassName('ctbox');
 var title=ctboxs.getElementsByClassName('title');
-var contentWidth=590;
+var contentHeight=470;
 var titleWidth=60;
 for(var i=0;i<title.length;i++){
-   
     title[i].index=i;
     title[i].onclick=function(){
         n=this.index;
         for(var i=0;i<ctbox.length;i++){
             if(i<= n){
-                ctbox[i].style.left=i*titleWidth+'px';
+                ctbox[i].style.top=i*titleWidth+'px';
             }else{
-                ctbox[i].style.left=i*titleWidth+contentWidth+'px';
+                ctbox[i].style.top=i*titleWidth+contentHeight+'px';
             }
-            
         }
     }
 }
